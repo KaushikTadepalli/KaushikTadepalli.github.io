ce0305e936f8f8c4cbaf6e78ab6edd7904f8ca6b
KaushikTadepalli <136290434+KaushikTadepalli@users.noreply.github.com>
2024-05-19 18:43:26 -0700

    Update file.js

--- file.js
+++ file.js
@@ -1,28 +1,59 @@
-const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-let interval = null;
-
-document.querySelector("h1").onmouseover = event => {  
-  let iteration = 0;
-  
-  clearInterval(interval);
-  
-  interval = setInterval(() => {
-    event.target.innerText = event.target.innerText
-      .split("")
-      .map((letter, index) => {
-        if(index < iteration) {
-          return event.target.dataset.value[index];
-        }
-      
-        return letters[Math.floor(Math.random() * 26)]
-      })
-      .join("");
-    
-    if(iteration >= event.target.dataset.value.length){ 
-      clearInterval(interval);
+const swiper = new Swiper(".swiper", {
+  direction: "horizontal",
+  loop: false,
+  speed: 2000,
+  slidesPerView: 4,
+  spaceBetween: 60,
+  mousewheel: true,
+  parallax: true,
+  centeredSlides: true,
+  effect: "coverflow",
+  coverflowEffect: {
+    rotate: 40,
+    slideShadows: true
+  },
+  autoplay: {
+    delay: 8000,
+    pauseOnMouseEnter: true
+  },
+  scrollbar: {
+    el: ".swiper-scrollbar"
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 60
+    },
+    600: {
+      slidesPerView: 2,
+      spaceBetween: 60
+    },
+    1000: {
+      slidesPerView: 3,
+      spaceBetween: 60
+    },
+    1400: {
+      slidesPerView: 4,
+      spaceBetween: 60
+    },
+    2300: {
+      slidesPerView: 5,
+      spaceBetween: 60
+    },
+    2900: {
+      slidesPerView: 6,
+      spaceBetween: 60
     }
-    
-    iteration += 1 / 3;
-  }, 30);
-}
+  }
+});
+
+document.querySelectorAll("nav ul li a").forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const slideId = this.getAttribute("href").substring(1);
+    const slideIndex = Array.from(
+      document.querySelectorAll(".swiper-slide")
+    ).findIndex((slide) => slide.id === slideId);
+    swiper.slideTo(slideIndex);
+  });
+});
